e1e3eeb0e95fe1808ae9749b0c37605d433ccaaf
Matt Johnson <matt.johnson@hulu.com>
2014-01-13 13:32:20 -0800

    Add cacheTimout support

--- lib/graphite_graph.rb
+++ lib/graphite_graph.rb
@@ -57,6 +57,7 @@ class GraphiteGraph
                    :placeholders => nil,
                    :area_alpha => nil,
                    :theme => nil,
+                   :cachetimeout => nil,
                    :unique_legend => nil}.merge(@overrides)
   end
 
@@ -311,6 +312,7 @@ class GraphiteGraph
     url_parts << "minXStep=#{properties[:min_x_step]}" if properties[:min_x_step]
     url_parts << "uniqueLegend=#{properties[:unique_legend]}" if properties[:unique_legend]
     url_parts << "template=#{properties[:theme]}" if properties[:theme]
+    url_parts << "cacheTimeout=#{properties[:cachetimeout]}" if properties[:cachetimeout]
 
     target_order.each do |name|
       target = targets[name]
